b271b8f394931bd4de87a18f5b28c4b43b16ce3f
赵瑞 王 <748339596@qq.com>
2022-09-12 22:14:31 +0800

    still working on it。。。

--- train_model.py
+++ train_model.py
@@ -1,9 +1,12 @@
 import numpy as np
 import cv2
+import torch
+import model
 
 if __name__=='__main__':
     data=np.load('data/data_train_0.npy',allow_pickle=True)
-    cv2.imshow('p',data[100][0])
-    cv2.waitKey(0)
-    x=data[:,1]
-    print(x[:])
+    #cv2.imshow('p',data[100][0])
+    #cv2.waitKey(0)
+    #x=data[:,1]
+    #print(x[:])
+    model.test()
